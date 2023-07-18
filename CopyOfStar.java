ee47d70b5c2ca904302535501bdec01d2002c658
INDIHARSHA <110756813+INDIHARSHA@users.noreply.github.com>
2023-07-18 09:07:17 +0530

    Add files via upload

--- CopyOfStar.java
+++ CopyOfStar.java
@@ -3,32 +3,27 @@ class CopyOfStar{
         
         int row=4;
         int col=4;
-           // *  *  *  *  *  *  
-             // *  *  *  *  *  
-               // *  *  *  *  
-                 // *  *  *  
-                   // *  *  
-                     // *  
-        for(int i=-1;i<=row;i++){
-            
-            
-            
-             for(int jv=0;jv<=i+1;jv++){
-                System.out.print("  ");
-            
-            }
-           for(int j=row+1;j>i;j--){
-               
-                System.out.print(" * ");
-             }
-             System.out.println(" ");
-            
-        
-        } 
+           
          
             
         
+         // *
+         // * *
+         // * * *
+         // * * * *
+         // * * * * *
+         // * * * * * *
         
+        for(int i=-1;i<=row;i++){
+            System.out.println();
+        
+            for(int j=0;j<=i+1;j++){
+                System.out.print(" *");
+            
+            }
+            
+        }
+            
     
     
     
